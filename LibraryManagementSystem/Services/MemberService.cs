ce8c78d4abf613970926630e2e5ceffa66e3fce1
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-09 10:54:01 +0600

    feat: Add Member Service with two methods Register and display members

---
+++ LibraryManagementSystem/Services/MemberService.cs
@@ -0,0 +1,32 @@
+﻿namespace LibraryManagementSystem.Services;
+
+public class MemberService
+{
+    private static readonly List<Member> _members = new List<Member>();
+    
+    public static void MemberRegister()
+    {
+
+        Member m = new Member();
+
+        Console.WriteLine("EnterName:");
+        m.name = Console.ReadLine();
+        
+        Console.WriteLine("Enter Contact Number");
+        m.ContactNumber = Console.ReadLine();
+        
+        
+        _members.Add(m);
+
+    }
+
+    public static void DisplayMembers()
+    {
+        foreach (var p in _members)
+        {
+            Console.WriteLine($"Name: {p.name} | Contact Number {p.ContactNumber}");
+        }
+    }
+    
+    
+}
