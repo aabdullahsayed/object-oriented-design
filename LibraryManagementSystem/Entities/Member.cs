ffa804a02129f797a5a8967eef148aa300f41e64
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-09 11:12:58 +0600

    Add Member id and Member Constructor

--- LibraryManagementSystem/Entities/Member.cs
+++ LibraryManagementSystem/Entities/Member.cs
@@ -1,7 +1,19 @@
-﻿namespace LibraryManagementSystem;
+﻿using System.Net;
+
+namespace LibraryManagementSystem;
 
 public class Member
 {
+    public int ID { get; set; }
     public string name { get; set; }
     public string ContactNumber { get; set; }
+    
+    private static int idd = 1;
+
+    public Member()
+    {
+        ID = idd++;
+    }
+    
+    
 }
