446b0500dcdf5a7802b13db5ed073b92ed0fba94
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-09 10:54:47 +0600

    Add two entities with required properties

---
+++ LibraryManagementSystem/Entities/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagementSystem;
+
+public class Book
+{
+    public int ID { get; set; }
+    public string author { get; set; }
+    public string title { get; set; }
+}
---
+++ LibraryManagementSystem/Entities/Member.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryManagementSystem;
+
+public class Member
+{
+    public string name { get; set; }
+    public string ContactNumber { get; set; }
+}
