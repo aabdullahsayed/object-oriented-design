6fffac76d9754a96564943aead8482699d7b58cb
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-12 22:37:25 +0600

    Library Service

--- LibraryManagementSystem/Services/BookService.cs
+++ LibraryManagementSystem/Services/BookService.cs
@@ -2,5 +2,30 @@
 
 public class BookService
 {
-    
+    private static readonly List<Book> _books = new List<Book>();
+
+    public static void AddBook()
+    {
+        
+        Book b = new Book();
+
+        Console.WriteLine("Enter Book Title: ");
+        b.title = Console.ReadLine();
+        
+        Console.WriteLine("Enter Book Author");
+        b.author = Console.ReadLine();
+
+        _books.Add(b);
+
+    }
+
+    public static void ShowBooks()
+    {
+        foreach (var book in _books)
+        {
+            Console.WriteLine($" ID {book.ID} Title: {book.title} Author: {book.author}");
+        }
+    }
+
+
 }
